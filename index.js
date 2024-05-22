2ff25504bdfac36fda7d414490057620199a716b
mrtnd <mrtndimitrov@gmail.com>
2024-05-22 17:24:52 +0300

    Signed-off-by: mrtnd <mrtndimitrov@gmail.com>

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ const puppeteer = require('puppeteer');
 const signale = require('signale');
 const ora = require('ora');
 const args = require('args-parser')(process.argv);
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
 (async () => {
   let spinner;
   try {
     signale.start(`html2image v${pkg.version}`);
 
-    if (!args.url) {
-      return signale.error('No url arg provided');
+    if (!args.url && !args.src) {
+      return signale.error('No url or src arg provided');
     }
     if (!args.dest) {
       return signale.error('No dest arg provided');
@@ -23,6 +26,7 @@ const args = require('args-parser')(process.argv);
     if (!args.width) {
       args.height = 600;
     }
+
     spinner = ora('Starting puoppeteer browser').start();
     const browser = await puppeteer.launch({
       headless: true,
@@ -38,22 +42,54 @@ const args = require('args-parser')(process.argv);
     }
     spinner.succeed();
     spinner = ora('Loading page').start();
-    await page.goto(args.url, {waitUntil: 'domcontentloaded'});
+    if (args.url) {
+      await page.goto(args.url, {waitUntil: 'domcontentloaded'});
+    } else if (args['local-server']) {
+      if (!args.port) {
+        args.port = 3000;
+      }
+      const app = express();
+      let filename = 'index.html';
+      if(fs.lstatSync(args.src).isDirectory()) {
+        app.use(express.static(args.src));
+        if (args.basedir) {
+          app.use(`/${args.basedir}`, express.static(args.src));
+        }
+      } else {
+        app.use(express.static(path.dirname(args.src)));
+        if (args.basedir) {
+          app.use(`/${args.basedir}`, express.static(path.dirname(args.src)));
+        }
+        filename = path.basename(args.src);
+      }
+      app.listen(args.port, async () => {
+        await page.goto(`http://localhost:${args.port}/${filename}`, {waitUntil: 'domcontentloaded'});
+      });
+    } else {
+      await page.goto(`file://${args.src}`, {waitUntil: 'domcontentloaded'});
+    }
     await page.waitForSelector('body');
     spinner.succeed();
     spinner = ora('Taking screenshot').start();
+    await sleep(3000);
     await page.screenshot({
       type: 'jpeg',
       path: args.dest,
       fullPage: false,
     });
-    await browser.close();
+    //await browser.close();
     spinner.succeed();
   } catch (err) {
     if (spinner) {
       spinner.stop();
     }
     signale.error(err);
-    process.exit();
   }
+  process.exit();
 })();
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
